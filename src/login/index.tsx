b2a7e2b73141374b591e6b46ce20f1a16fd54dce
chrizLopez <yambachristianjee@gmail.com>
2024-06-20 09:52:35 +0800

    added  login UI

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import styles from './styles';
 
@@ -9,12 +9,24 @@ const Login = () => {
     <View style={styles.container}>
       <ImageBackground source={imgBg} style={{ width: '100%', height: '100%', position: 'absolute' }} />
       <View style={styles.contentView}>
-        <Text>Login Page</Text>
 
         <View style={styles.inputView}>
-          <Text style={styles.inputLabel}>Username</Text>
+          <Text style={styles.inputLabel}>Email</Text>
           <TextInput style={styles.input} />
         </View>
+
+        <View style={styles.inputView}>
+          <Text style={styles.inputLabel}>Password</Text>
+          <TextInput style={styles.input} secureTextEntry />
+        </View>
+
+        <TouchableOpacity
+            style={styles.loginBtn}
+          >
+            <Text style={styles.loginTxt}>
+              Login
+            </Text>
+          </TouchableOpacity>
       </View>
     </View>
   )
--- src/login/styles.ts
+++ src/login/styles.ts
@@ -1,36 +1,55 @@
-import {EStyleSheet} from '../config/EStyleSheet';
+import { EStyleSheet } from "../config/EStyleSheet";
 
 const styles = EStyleSheet.create({
+  loginTxt: {
+    fontSize: "16rem",
+    fontWeight: "500",
+    color: "#fff",
+  },
+  loginBtn: {
+    backgroundColor: "#f64082",
+    paddingVertical: "15rem",
+    // paddingHorizontal: "20rem",
+    borderRadius: "50rem",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   contentView: {
-    backgroundColor: '#2b2b2b50',
-    padding: '10rem',
-    borderRadius: '20rem',
-    width: '80%',
+    backgroundColor: "#2b2b2b50",
+    padding: "10rem",
+    borderRadius: "20rem",
+    width: "80%",
   },
   bgImage: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute'
+    width: "100%",
+    height: "100%",
+    position: "absolute",
   },
   input: {
-    borderRadius: '5rem',
-    width: '100%',
-    marginBottom: '10rem',
-    borderWidth: '1rem',
-    borderColor: '#fff',
-    padding: '10rem',
+    width: "100%",
+    borderBottomWidth: "1rem",
+    borderBottomColor: "#fff",
+    padding: "10rem",
+    color: "#fff",
   },
   inputLabel: {
-    color: '#fff',
+    color: "#fff",
+    marginBottom: "5rem",
+    fontSize: "16rem",
   },
   inputView: {
-    marginVertical: '20rem',
+    marginVertical: "20rem",
+    // backgroundColor: "#fff",
   },
-})
+  loginHeader: {
+    color: "#fff",
+    fontSize: "24rem",
+    marginBottom: "20rem",
+  },
+});
 
 export default styles;
